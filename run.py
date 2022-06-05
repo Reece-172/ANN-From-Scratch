a3e543525111fd2efb577403885262de99d2a2a5
ebenezergelo <2337994@students.wits.ac.za>
2022-06-05 22:21:46 +0200

    Print predictions -> stdout

--- run.py
+++ run.py
@@ -13,6 +13,10 @@ def sigmoid(Z):
 def get_predictions(A3): 
     return np.argmax(A3,0)
 
+def get_accuracy(predictions, Y):
+    accuracy = np.sum(predictions == Y) / Y.size
+    return accuracy * 100
+
 def forward_prop(X, w1, b1, w2, b2, w3, b3): #forward propagation
     Z1 = np.dot(w1, X) + b1
     A1 = sigmoid(Z1)
@@ -31,11 +35,19 @@ b3 = np.loadtxt('b3.txt')
 b1 = b1.reshape(20,1)
 b2 = b2.reshape(20,1)
 b3 = b3.reshape(10,1)
-
+labels = np.loadtxt('labels.txt')
+labels = labels.reshape(2000,1)
 eigens = np.loadtxt("eigens.txt")
-data = np.loadtxt(sys.stdin)
-data = normalize(data)
-scaled_data = np.dot(data, eigens)
-test_data = scaled_data.reshape(50,1)
-output = get_predictions(forward_prop(test_data, w1, b1, w2, b2, w3, b3))
-sys.stdout.write(str(output))
+allData = np.loadtxt(sys.stdin)
+allData = normalize(allData)
+predictions = []
+for data in allData:
+    scaled_data = np.dot(data, eigens)
+    test_data = scaled_data.reshape(50,1)
+    output = get_predictions(forward_prop(test_data, w1, b1, w2, b2, w3, b3))
+    predictions.append(output)
+    # sys.stdout.write(str(output))
+
+predictions = np.array(predictions)
+print(get_accuracy(predictions, labels))
+#python3 run.py<test.txt
